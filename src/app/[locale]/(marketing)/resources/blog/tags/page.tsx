6a120f0e71d7f0eee797fe86ea5cb9226ba7e2a3
ryanngab <firdausriyan402@gmail.com>
2025-03-11 20:23:54 +0700

    Update : Prodjects

--- src/app/[locale]/(marketing)/resources/blog/tags/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllTags } from "@/lib/markdown";
-import { TagsList } from "./tags-list";
-
-export default async function TagsPage() {
-  const tags = await getAllTags();
-  return <TagsList tags={tags} />;
-}
